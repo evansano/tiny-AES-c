a0b2a3a8e9a8b786c62617f0648cd95d2186e30c
evansano <evanksano@gmail.com>
2018-04-29 17:36:32 -0700

    Switching to full filesize array

--- test.c
+++ test.c
@@ -8,10 +8,10 @@
 #define LOOPS 50
 #include "aes.h"
 
-// int arrSizes[8] = {1024, 8192, 65536, 1048576, 5242880, 10485760, 104857600, 1073741824};
-// char* arrSizeHuman[8] = {"1 kB", "8 kB", "64 kB", "1 MB" "5 MB", "10 MB", "100 MB", "1 GB"};
-int arrSizes[3] = {1024, 8192, 65536};
-char* arrSizeHuman[3] = {"1 kB", "8 kB", "64 kB"};
+int arrSizes[8] = {1024, 8192, 65536, 1048576, 5242880, 10485760, 104857600, 1073741824};
+char* arrSizeHuman[8] = {"1 kB", "8 kB", "64 kB", "1 MB" "5 MB", "10 MB", "100 MB", "1 GB"};
+// int arrSizes[3] = {1024, 8192, 65536};
+// char* arrSizeHuman[3] = {"1 kB", "8 kB", "64 kB"};
 static void phex(uint8_t* str);
 
 
@@ -68,7 +68,6 @@ int main(int argc, char **argv)
             }
             
             MPI_Bcast(&local_num_blocks, 1, MPI_LONG_LONG, 0, comm);
-            printf("[%d/%d] received: %llu\n", my_rank, comm_sz, local_num_blocks);
             local_in = malloc(AES_BLOCKLEN*local_num_blocks*sizeof(int));
             MPI_Scatter(in, AES_BLOCKLEN*local_num_blocks, MPI_INT,
                           local_in, AES_BLOCKLEN*local_num_blocks, MPI_INT, 0, comm);
